7e08f218df9d099fa9b7bf0348795588b532d1e9
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-04 13:39:49 -0500

    feat: :sparkles: Implementation of Webhook

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/webhook", async context =>
+{
+    var requestBody = await context.Request.ReadFromJsonAsync<WebhookPayload>();
+    Console.WriteLine($"Header: {requestBody.Header}, Body: {requestBody.Body}");
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("Webhook Ack!");
+});
 
 app.Run();
+
+public record WebhookPayload (string Header, string Body);
